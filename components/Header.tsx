ad49ea3805d4e75b67b1bafa2d429f1a7bcc979f
Kalu Daniel <krayzhee419@gmail.com>
2024-05-11 01:44:27 +0100

    * Updated header component to include capitalized welcome message on dashboard page

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = ({ currentPage }) => {
     return (
-        <header className="flex items-center justify-between p-6 ">
+        <header className="flex items-center justify-between p-[30px] ">
             <div className="font-bold text-[20px]">
                 <Link href="/"> K H A Y </Link>
             </div>
@@ -20,7 +20,7 @@ export const Header: React.FC<HeaderProps> = ({ currentPage }) => {
                     </Link>
                 )}
                 {currentPage === "login" && <div />}
-                {currentPage === "dashboard" && <div>Welcome back daniel</div>}
+                {currentPage === "dashboard" && <div>Welcome Back Daniel</div>}
             </div>
         </header>
     );
