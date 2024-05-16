0daea79914ffcd71176fae05d949a59bbf328595
Kalu Daniel <krayzhee419@gmail.com>
2024-05-16 19:55:14 +0100

    added fund component with form and button created and added fund reducer to store."

---
+++ lib/fund/fund.ts
@@ -0,0 +1,40 @@
+'use client';
+import {  PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+export interface fundState {
+  amount: number;
+}
+
+let storedValue = 0;
+if (typeof window !== "undefined") {
+  storedValue = parseInt(localStorage.getItem("amount") || "0", 10);
+}
+const initialValue = storedValue;
+
+const initialState: fundState = {
+  amount: initialValue,
+};
+
+
+const fundSlice = createSlice({
+  name: 'funds',
+  initialState,
+  reducers: {
+    updateFunds: (state, action: PayloadAction<number>) => {
+      state.amount += action.payload;
+      if (typeof window !== "undefined") {
+        localStorage.setItem("amount", state.amount.toString());
+      }
+    },
+    resetAmount: (state) => {
+      state.amount = 0
+      if (typeof window !== "undefined") {
+        localStorage.setItem("amount", "0");
+      }
+   
+    }    
+  },
+});
+
+export const { updateFunds, resetAmount } = fundSlice.actions
+export default fundSlice.reducer;
--- lib/store.ts
+++ lib/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./user/user";
-
+import fundSliceReducer from "./fund/fund"
 export const store = configureStore({
 	reducer: {
-		username: userSliceReducer
-	}
+		username: userSliceReducer,
+	   amount:fundSliceReducer
+
+	},
+	
 });
 
 
